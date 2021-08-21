eb13ce126a5745b77954ac7644324b64b3168c8c
Dawn Nguyen <lophocpzo@gmail.com>
2021-08-21 16:33:16 +0700

    Code list exercise but in positive case

--- nguyenhoangdang/19_08/List exercise.kts
+++ nguyenhoangdang/19_08/List exercise.kts
@@ -1,4 +1,4 @@
-fun main(){
+fun main() {
     val list = mutableListOf<Int>()
     println("Hãy nhập từng số rồi enter nhé <3")
     var input: Int? = readLine()?.toIntOrNull()
@@ -7,11 +7,15 @@ fun main(){
         input = readLine()?.toIntOrNull()
     }
     val markArray = IntArray(list.maxOrNull()!! + 1)
-    for (i in list){
-        markArray[i] ++
+    for (i in list) {
+        markArray[i]++
     }
     val mostAppearCount: Int = markArray.maxOrNull()!!
-    val mostAppearValue: Int = markArray.indexOf(mostAppearCount)
+    val mostAppearIndex = mutableListOf<Int>()
+    markArray.forEachIndexed { index, i ->
+        if (i == mostAppearCount) mostAppearIndex.add(index)
+    }
+    val mostAppearValue: Int = list.first { it in mostAppearIndex }
     println("Bạn đã nhập các số theo thứ tự sau: $list")
     println("Giá trị lớn nhất trong các số bạn vừa nhập là: ${list.maxOrNull()}")
     println("Giá trị nhỏ nhất trong các số bạn vừa nhập là: ${list.minOrNull()}")

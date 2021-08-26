402953d228e2593e2211e10a8a5e7e3e4860e8ed
thuankhang <matthuanphong@gmail.com>
2021-08-26 10:55:13 +0700

    Fix : Homework 26/8

--- dinhthuankhang/26_08/Average of IntArray.kt
+++ dinhthuankhang/26_08/Average of IntArray.kt
@@ -13,14 +13,25 @@ fun main(){
                 i++
             }
         }
-        val sum = arrayNumber.sum()
+        var sum = arrayNumber.sum()
         val size = arrayNumber.size
-        val average = sum / size
-        var averageDecimal = (sum - average * size) * 1000 / size
-        do {
-            averageDecimal /= 10
-        } while (averageDecimal % 10 == 0 || averageDecimal / 10 != 0)
-        println("Average is $average,$averageDecimal")
+        if (sum >= 0) {
+            val average = sum / size
+            var averageDecimal = (sum - average * size) * 1000 / size
+            do {
+                averageDecimal /= 10
+            } while (averageDecimal % 10 == 0 || averageDecimal / 10 != 0)
+            println("Average is $average,$averageDecimal")
+        }
+        else {
+            sum = -sum
+            val average = sum / size
+            var averageDecimal = (sum - average * size) * 1000 / size
+            do {
+                averageDecimal /= 10
+            } while (averageDecimal % 10 == 0 || averageDecimal / 10 != 0)
+            println("Average is -$average,$averageDecimal")
+        }
     }
     else print("Error")
 }
